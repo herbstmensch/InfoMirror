ea851d2ccf679c9976886554e2ba427cfa75becb
Tim Herbst <mail@timherbst.de>
2015-02-17 14:49:20 +0100

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -83,8 +83,7 @@ function checkVersion() {
     $.getJSON('githash.php', {}, function(json, textStatus) {
         if (json) {
             if (json.gitHash != gitHash) {
-                window.location.reload();
-                window.location.href = window.location.href;
+                window.location.reload(true);
             }
         }
     });
